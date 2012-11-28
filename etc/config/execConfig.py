0d55b4945babb4934d7b037266810bd205f6f25f
Greg Daues <daues@lsst14.ncsa.illinois.edu>
2012-11-28 14:01:45 -0600

    Initial file set for ctrl_platform_lonestar for condor glidein based job submission to TACC Lonestar cluster.

---
+++ etc/config/execConfig.py
@@ -0,0 +1,4 @@
+root.platform.localScratch = "$HOME/condor_scratch"
+root.platform.defaultRoot = "/scratch/00342/$USER_NAME/defaultRoot"
+root.platform.dataDirectory = "/scratch/00342/ux453102/stripe82/dr7/runs"
+root.platform.fileSystemDomain = "tacc.utexas.edu"
---
+++ etc/config/pbsConfig.py
@@ -0,0 +1,6 @@
+root.platform.queue = "development"
+root.platform.email = "#$ -m be"
+root.platform.scratchDirectory = "$USER_HOME/condor_scratch"
+root.platform.loginHostName = "login1.ls4.tacc.utexas.edu"
+root.platform.utilityPath = "/opt/sge6.2/bin/lx24-amd64"
+root.platform.totalCoresPerNode=12
